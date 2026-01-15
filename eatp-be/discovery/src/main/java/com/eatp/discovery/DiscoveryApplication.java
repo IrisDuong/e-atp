85e73e3ee755eb7a0029ad2dc33784ed99b8d786
Manhnk2 <manhtt09@gmail.com>
2026-01-15 11:26:12 +0700

    thay doi load config cua discovery

--- eatp-be/discovery/src/main/java/com/eatp/discovery/DiscoveryApplication.java
+++ eatp-be/discovery/src/main/java/com/eatp/discovery/DiscoveryApplication.java
@@ -1,16 +1,20 @@
 package com.eatp.discovery;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryApplication.class, args);
 	}
