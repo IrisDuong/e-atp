de3368d723ed72ec381bd40c11aad172bf628f26
Manhnk2 <manhtt09@gmail.com>
2026-01-27 15:18:47 +0700

    init notification service

---
+++ eatp-be/notification/src/test/java/com/eatp/notification/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eatp.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
